009a0dbe7d17ecb3b64eb0a963937b043b51378c
Dillongrow <57330905+Dillongrow@users.noreply.github.com>
2020-03-17 19:09:08 -0700

    Third Commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,18 +1,47 @@
 // from data.js
 var tableData = data;
 
-var tbody = d3.select("tbody");
-
 console.log(tableData);
 
-tableData.forEach(function(sighting) {
-    console.log(sighting);
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(function([key, value]) {
-      console.log(key, value);
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
+var tbody = d3.select("tbody");
 
-var button = d3.select("#button");
+tableData.forEach(function(sighting) {
+        console.log(sighting);
+            var row = tbody.append("tr");
+        Object.entries(sighting).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+        cell.text(value);
+         }); 
+});
+
+
+var filter = d3.select("#filter-btn");
+
+filter.on("click", function() {
+
+    tbody.html("");
+
+    var inputField = d3.select("#datetime");
+    var inputValue = inputField.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach(function(selections) {
+
+        console.log(selections);
+       
+        var row = tbody.append("tr");
+        
+        Object.entries(selections).forEach(function([key, value]) {
+            console.log(key, value);
+
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});

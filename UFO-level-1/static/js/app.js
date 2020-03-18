a47899c1225064788322e73189fdc901f239fc22
Dillongrow <57330905+Dillongrow@users.noreply.github.com>
2020-03-17 17:40:50 -0700

    Second Commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(tableData);
+
+tableData.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+var button = d3.select("#button");
